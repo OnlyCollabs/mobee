e584559342d440005ea2015c30b2c6abe8c7fcce
Rohit Singh <rohit14kvs@gmail.com>
2022-03-20 14:15:27 +0530

    Api data fetched

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ import React, { useEffect, useState } from "react";
 
 const App = () => {
   const [movie, setMovie] = useState("");
-  const [movieData, setMovieData] = useState("");
   const maxPage = 500;
   const maxMovieNo = 19;
 
@@ -17,27 +16,14 @@ const App = () => {
     return response.json();
   };
 
-  const fetchMovieDetails = async () => {
-    const movieUrl = `https://api.themoviedb.org/3/movie/${movie.id}?api_key=f556ec5937fcffab8354d8415d6db8f8&language=en-US`;
-    const details = await fetch(movieUrl, {
-      headers: {
-        "Content-Type": "application/json",
-      },
-    });
-    return details.json();
-  };
-
   useEffect(() => {
     const selectMovie = Math.floor(Math.random() * maxMovieNo) + 1;
-    // console.log(selectMovie);
     fetchData().then((data) => setMovie(data.results[selectMovie]));
   }, []);
 
-  fetchMovieDetails().then((data) => console.log(data));
+  console.log(movie);
 
-  console.log(movieData);
-
-  return <div>App</div>;
+  return <div>{movie.title}</div>;
 };
 
 export default App;
