432e3919647d2f45de30a7dbb525a238987943bf
Rohit Singh <rohit14kvs@gmail.com>
2022-03-20 13:52:54 +0530

    TMDB api

--- src/App.js
+++ src/App.js
@@ -1,25 +1,43 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App = () => {
+  const [movie, setMovie] = useState("");
+  const [movieData, setMovieData] = useState("");
+  const maxPage = 500;
+  const maxMovieNo = 19;
+
+  const fetchData = async () => {
+    const pageNo = Math.floor(Math.random() * maxPage) + 1;
+    const englishUrl = `https://api.themoviedb.org/3/discover/movie?api_key=f556ec5937fcffab8354d8415d6db8f8&language=en-US&sort_by=popularity.desc&page=${pageNo}`;
+    const response = await fetch(englishUrl, {
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+    return response.json();
+  };
+
+  const fetchMovieDetails = async () => {
+    const movieUrl = `https://api.themoviedb.org/3/movie/${movie.id}?api_key=f556ec5937fcffab8354d8415d6db8f8&language=en-US`;
+    const details = await fetch(movieUrl, {
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+    return details.json();
+  };
+
+  useEffect(() => {
+    const selectMovie = Math.floor(Math.random() * maxMovieNo) + 1;
+    // console.log(selectMovie);
+    fetchData().then((data) => setMovie(data.results[selectMovie]));
+  }, []);
+
+  fetchMovieDetails().then((data) => console.log(data));
+
+  console.log(movieData);
+
+  return <div>App</div>;
+};
 
 export default App;
