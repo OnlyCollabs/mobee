902e5fb016f385c8b8f885077cdd405a46dc1026
Rohit Singh <rohit14kvs@gmail.com>
2022-04-05 16:03:57 +0530

    Context Updated

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import React from "react";
 import MovieBoard from "./components/MovieBoard/MovieBoard";
+import { GameContextProvider } from "./Context/GameContextProvider";
 
 const App = () => {
   return (
-    <div className="h-screen flex items-center justify-center">
-      <MovieBoard />
-    </div>
+    <GameContextProvider>
+      <div className="h-screen flex items-center justify-center">
+        <MovieBoard />
+      </div>
+    </GameContextProvider>
   );
 };
 
